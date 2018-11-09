3956e4b2f57802b5387e75803d4095a0e62960fe
Bernhard Meindl <bernhard.meindl@statistik.gv.at>
2018-11-09 10:42:33 +0100

    convert to json (for shiny app) implemented

--- R/sdcHier_convert.R
+++ R/sdcHier_convert.R
@@ -5,12 +5,45 @@
 #' - **"data.frame"**: a `data.frame` with two columns. The first columns contains a string containing as many `@` as the level
 #' of the node in the string (e.g `@` corresponds to the overall total while `@@` would be all codes contributing to the total.
 #' The second column contains the names of the levels.
-#' - **"json"**: json format
+#' - **"json"**: json format suitable as input for shiny Tree
 #' @export
 #' @md
 #' @examples
 #' ## for examples, see ?sdcHier_create
 sdcHier_convert <- function(h, format="data.frame") {
+  h_to_json <- function(h) {
+    write.json.row <- function(id, parent, text, opened=TRUE, disabled=FALSE, selected=FALSE) {
+      stopifnot(is_scalar_character(id))
+      stopifnot(is_scalar_character(parent))
+      stopifnot(is_scalar_character(text))
+      op <- ifelse(opened==TRUE, "true","false")
+      dis <- ifelse(disabled==TRUE, "true","false")
+      sel <- ifelse(selected==TRUE, "true","false")
+      js <- paste0("{",dQuote("id"),":",dQuote(id),",",dQuote("parent"),":",dQuote(parent),",",dQuote("text"),":",dQuote(text))
+      js <- paste0(js, ",",dQuote("state"),":{",dQuote("opened"),":",op,",",dQuote("disabled"),":",dis,",",dQuote("selected"),":",sel,"}}")
+      js
+    }
+
+    df <- ToDataFrameTypeCol(h)
+
+    if (!is.data.frame(df)) {
+      js <- paste0('[]')
+      return(js)
+    }
+
+    df[[1]] <- "#"
+    js <- "["
+    for (i in 2:ncol(df)) {
+      sub <- unique(df[,c(i-1, i)])
+      sub <- sub[!is.na(sub[[2]]),]
+      for (j in 1:nrow(sub)) {
+        js <- paste0(js, write.json.row(id=sub[[2]][j], parent=sub[[1]][j], text=sub[[2]][j]),",")
+      }
+    }
+    js <- paste0(js,"]")
+    sub(",\\]","\\]", js)
+  }
+
   h_is_valid(h)
 
   stopifnot(is_scalar_character(format))
@@ -26,7 +59,7 @@ sdcHier_convert <- function(h, format="data.frame") {
     return(df)
   }
   if (format=="json") {
-    # todo
+    return(h_to_json(h))
   }
   stop(paste("Error in sdcHier_convert()"), call.=FALSE)
 }
