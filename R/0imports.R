b10c4988d5b6f00526c8ad682790622cefd36af7
Bernhard Meindl <bernhard.meindl@statistik.gv.at>
2018-11-09 10:53:23 +0100

    update imports and documentation

--- R/0imports.R
+++ R/0imports.R
@@ -1,8 +1,8 @@
 #' @importFrom shiny runApp shinyApp shinyOptions stopApp
 #' @import shinyjs
 #' @import shinyTree
-#' @importFrom data.tree ToDataFrameTypeCol FromDataFrameTable Traverse FindNode Node
+#' @importFrom data.tree ToDataFrameTypeCol FromDataFrameTable Traverse
+#' @importFrom data.tree FromDataFrameNetwork FindNode Node
 #' @importFrom jsonlite fromJSON toJSON
-#' @importFrom sdcTable add_nodes
 #' @importFrom rlang is_scalar_logical is_scalar_character is_integerish
 NULL
