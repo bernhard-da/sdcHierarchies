da3ba4e35bdb44869e18b1f733019c7ba6d8980e
bernhard-da <bernhard.meindl@data-analysis.at>
2019-01-23 00:07:43 +0100

    add additional sanity check

--- R/sdcHier_add.R
+++ R/sdcHier_add.R
@@ -16,12 +16,17 @@ sdcHier_add <- function(h, refnode, node_labs) {
   stopifnot(is_scalar_character(refnode))
   stopifnot(is.character(node_labs))
 
+  if (is.null(FindNode(h, refnode))) {
+    stop("The reference node does not exist!\n", call. = FALSE)
+  }
   if (refnode %in% node_labs) {
     stop(paste("at least one leaf-name equals", shQuote(refnode)), call. = FALSE)
   }
+
   res <- sapply(node_labs, function(x) {
     sdcHier_info(h, node_labs = x)$exists
   })
+
   nn <- node_labs
   for (i in 1:length(nn)) {
     if (res[i] == TRUE) {
